d70ee0804c5707055eec7823de931721472a80d3
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-04-23 17:49:53 +0900

    src

---
+++ src/codeup100/c1030.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+import java.math.BigDecimal;
+
+public class c1030 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		BigDecimal bigDecimal = sc.nextBigDecimal();
+		sc.close();
+		
+		System.out.println(bigDecimal);
+	}
+}
