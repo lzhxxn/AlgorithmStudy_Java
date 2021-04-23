616ac228489ecb01393811c207a39d198492d30d
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-04-23 17:46:51 +0900

    Create c1028.java

---
+++ src/codeup100/c1028.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class c1028 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 입력: ");
+		long num = sc.nextLong();
+		sc.close();
+		
+		System.out.println(num);
+	}
+}
