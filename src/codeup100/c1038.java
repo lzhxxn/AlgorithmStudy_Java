1d4f97b3a63931ad37a7197b1236237c1f7e6966
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-04-25 17:45:41 +0900

    src

---
+++ src/codeup100/c1038.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class c1038 {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		
+		System.out.printf("%.0f", a+b);
+	}
+}
