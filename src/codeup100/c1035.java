7e5887f970911d65bdc7fa83cab9fd54e3cc97ed
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-04-25 17:34:33 +0900

    src

---
+++ src/codeup100/c1035.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class c1034 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("8진수로 변환할 16진수 입력: ");
+		int num = sc.nextInt(16); // 8진수 정수 입력
+		sc.close();
+		
+		System.out.printf("%o", num);
+	}
+}
