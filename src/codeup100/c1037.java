4e62c9388b33e2685f974f328e6163dcb37f397a
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-04-25 17:40:09 +0900

    src

---
+++ src/codeup100/c1037.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class c1037 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 입력: ");
+    
+    int num = sc.nextInt();
+		sc.close();
+		
+		System.out.printf((char)num);
+	}
+}
