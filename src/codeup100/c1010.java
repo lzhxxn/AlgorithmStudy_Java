7d3373ae04ff442f388aad9436d697c55b890875
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-05-08 19:08:14 +0900

    src

--- src/codeup100/c1010.java
+++ src/codeup100/c1010.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
  
 public class c1010 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
 
         int number = sc.nextInt();
         System.out.println(number);
