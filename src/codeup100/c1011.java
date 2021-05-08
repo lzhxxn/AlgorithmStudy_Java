d56ff19f9e2c2ac62d4fc2b9f3cc1c12f904748c
LEE JIHOON <75344302+lzhxxn@users.noreply.github.com>
2021-05-08 19:08:23 +0900

    src

--- src/codeup100/c1011.java
+++ src/codeup100/c1011.java
@@ -5,7 +5,7 @@ public class c1011 {
         Scanner sc = new Scanner(System.in);//Scanner 객체생성
 		//System.out.println("문자 입력 : ");
         char a = sc.next().charAt(0); //사용자로부터 입력 받기
-        System.out.println(a);//출력
+        System.out.println(a);//출력 
 		sc.close();//입력 종료 후 Scanner 닫기
     }
 }
